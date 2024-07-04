b86cab2ee9d76814e3995daa9cd03e3537b6713e
xtrm <oss@xtrm.me>
2024-07-05 00:26:57 +0200

    ✨ feature(frontend): add htmx example
    Signed-off-by: xtrm <oss@xtrm.me>

--- services/backend/website-service/website-service/main.py
+++ services/backend/website-service/website-service/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.utils import get_random_secret_key
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import path, re_path
 from .decorators import *
@@ -28,11 +29,18 @@ def template(template_name):
 
 
 @template('index.html')
-def home(request):
+def home(_):
     return locals()
+
+#TODO: move this to backend/api-service
+def test(_):
+    return HttpResponse("<p>fuck you :3</p>")
 
 
 urlpatterns = [
+    #TODO: move this to backend/api-service
+    path('api/v1/test', test, name='test'),
+
     re_path(r'^$', home, name='home'),
     re_path(r'.*', lambda _: redirect('home')),
 ]
