306acdfec552b4090d406c4468319e7916fced5f
killian <oss@xtrm.me>
2024-07-22 17:50:25 +0200

    🚧 wip: try to work out postcss
    Signed-off-by: killian <oss@xtrm.me>

--- services/website/frontend/index.js
+++ services/website/frontend/index.js
@@ -1,4 +1,6 @@
-import './tailwind.css'
-import * as htmx from 'htmx.org'
+import './index.css';
+import '/tailwind.css';
+import '/styles.scss';
+import * as htmx from 'htmx.org';
 
 window.htmx = htmx;
